4856c421fed4e5ad139c5cec74b1f21a8b3e2ea8
EvaHergar <eherng03@estudiantes.unileon.es>
2018-05-23 20:35:29 +0200

    Add files via upload

--- NoBloqueantes.c
+++ NoBloqueantes.c
@@ -3,28 +3,33 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define VECTOR_SLAVE_SIZE 1000
-#define VECTOR_MASTER_SIZE 100000
+#define VEC_SLAVE_SIZE 1000
+#define VEC_MASTER_SIZE 100000
 #define MESSAGE_TAG_0 0
+#define MESSAGE_TAG_1 1
 
 #define ID_MAESTRO 0
-#define VECTOR_MAX_VALUE 10000
+#define MAX_VALUE 10000
 
+int getMax(int * vector, int size);
 
 int main(int argc, char *argv[])
 {
-	int size, rank, i, from, to, ndat, part, tag, elementosRestantes, process_id, indiceCola;
-	int * vectorEsclavo;
-	int * vectorMaestro;
-	//servirá para saber que proceso es el siguiente al que le tego que enviar la información
-	int * colaProcesos;
-	long acumm = 0;
-	process_id = 1;
-	elementosRestantes = VECTOR_MASTER_SIZE;
-	indiceCola = 0;
 
-	MPI_Status info_0, info_1, info_2;
-	MPI_Request request_0, request_1, request_2;
+	/*=====================================================================================*
+	*		  							Datos necesarios 								   *
+	=======================================================================================*/
+	int size, rank, i, from, to, ndat, part, tag, restElems, max, source, globMax, seguir, datasize, maxIndex;
+	int * vector, * maximos;
+
+	restElems = VEC_MASTER_SIZE;
+	max = 0;
+	globMax = 0;
+	source = 0;
+	seguir = 1;
+	maxIndex = 0;
+	MPI_Status st_0, st_1, st_2, st_3, status, stStave;
+	MPI_Request req_0, req_1, req_2, req_3;
 	
 	srand(time(NULL));
 
@@ -32,91 +37,153 @@ int main(int argc, char *argv[])
 	MPI_Comm_size(MPI_COMM_WORLD,&size);
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 
-	/*======================================================================================																				   *
-	*		  				    Reserva de memoria para los vectores 					   *
-	=======================================================================================*/
-	vectorEsclavo = (int *) malloc(VECTOR_SLAVE_SIZE * sizeof(int));
-	if(vectorEsclavo == NULL){
-		printf("Error de reserva de memoria para el vector esclavo.\n");
-		return -1;
-	}
 
-
-	colaProcesos = (int *) malloc(size * sizeof(int));
-	if(colaProcesos == NULL){
-		printf("Error de reserva de memoria para la cola de procesos.\n");
-		return -1;
-	}
-
-	/*======================================================================================																				   *
+	/*=====================================================================================*
 	*		  			Rellenado de vectores con valores aleatorios 					   *
 	=======================================================================================*/
 	if (rank == ID_MAESTRO){
-		vectorMaestro = (int *) malloc(VECTOR_MASTER_SIZE * sizeof(int));
-		if(vectorMaestro == NULL){
+		vector = (int *) malloc(VEC_MASTER_SIZE * sizeof(int));
+		if(vector == NULL){
+			printf("Error de reserva de memoria para el vector del maestro.\n");
+			return -1;
+		}
+		
+		//Rellena el vector con valores aleatorios
+		for (i = 0; i < VEC_MASTER_SIZE; ++i)
+			vector[i] = rand()%(MAX_VALUE + 1);
+		printf("Vector del maestro rellenado.\n");
+		
+		maximos = (int *) malloc((VEC_MASTER_SIZE/size) * sizeof(int));
+		if(maximos == NULL){
 			printf("Error de reserva de memoria para el vector del maestro.\n");
 			return -1;
 		}	
-				//Rellena el vector con valores aleatorios
-		for (i = 0; i < N; ++i)
-			vectorMaestro[i] = rand()%VECTOR_MAX_VALUE;
+		//Rellena el vector con valores aleatorios
+		for (i = 0; i < (VEC_MASTER_SIZE/size); ++i)
+			maximos[i] = rand()%(MAX_VALUE + 1);	
+		
 
 	}else{
+		vector = (int *) malloc(VEC_SLAVE_SIZE * sizeof(int));
+		if(vector == NULL){
+			printf("Error de reserva de memoria para el vector esclavo.\n");
+			return -1;
+		}
 		//Los esclavos inicializan el vector a 0
-		for (i = 0; i < N; ++i)
-			vectorEsclavo[i] = 0;
+		for (i = 0; i < VEC_SLAVE_SIZE; ++i)
+			vector[i] = 0;
+		printf("Vector del proceso %d rellenado.\n", rank);
 	}
-
-	//inicializo la cola de procesos con 0, se almacenaran los procesos finalizados
-	for (i = 1; i <= size; ++i)
-			colaProcesos[i] = 0 ;
 
 	//Espero a que todos los procesos rellenen su vector
 	MPI_Barrier(MPI_COMM_WORLD); 
 
-	//Mientras queden elementos que mandar, los reparto entre los vectores
-	while(elementosRestantes != 0){
-		if(rank == ID_MAESTRO){
-		int isend_status = 0;
-		if(process_id > size){
-			process_id
-			indiceCola = (indiceCola + 1) % size;
+	/*=====================================================================================*
+	*		  							Envio y proceso de datos 						   *
+	=======================================================================================*/
+	
+	if(rank == ID_MAESTRO){
+		printf("----------------------------------------------------------------------------\n");
+		//el master manda los primeros elementos a los procesos
+		
+		for(i = 1; i < size; i++){
+			//Envia 1000 elementos de su vector
+			MPI_Isend(&vector[(i-1)*VEC_SLAVE_SIZE], VEC_SLAVE_SIZE, MPI_INT, i, 0, MPI_COMM_WORLD, &req_0);
+			MPI_Wait(&req_0, &st_0);
+			max = getMax(&vector[(i-1)*VEC_SLAVE_SIZE], VEC_SLAVE_SIZE);
+			printf("El master ha calculado el maximo del vector enviado al proceso %d: %d. \n", i, max);
+			restElems = restElems - VEC_SLAVE_SIZE;
 		}
-		//Envia 1000 elementos del vector del maestro
-		isend_status = MPI_Isend(vectorMaestro, VECTOR_SLAVE_SIZE, MPI_INT, process_id, MESSAGE_TAG_0, MPI_COMM_WORLD, &request_0);
-		MPI_Wait(&request_0, &info_0);
-		printf("El master envia elementos al proceso %d\n", colaProcesos[indiceCola]);
-		elementosRestantes = elementosRestantes - VECTOR_SLAVE_SIZE;
-		//paso al siguiente proceso
-		process_id = process_id + 1;
-		}else{
-			int max = 0;
-			int irec_status = 0;
-			int isend_status = 0;
-
-			//Recibimos los datos enviados por el maestro
-			irec_status = MPI_Irecv(vectorEsclavo, VECTOR_SLAVE_SIZE, MPI_INT, ID_MAESTRO, MESSAGE_TAG_0, MPI_COMM_WORLD, &request_1);
-			MPI_Wait(&request_1, &info_1);
+	}else{
+		//Los esclavos esperan a recibir vectores del maestro
+		
+		while(seguir){
+			//Comprobamos si hay algún mensaje pendiente
+			MPI_Probe(0, MPI_ANY_TAG, MPI_COMM_WORLD, &stStave);
 			
+			MPI_Get_count(&stStave, MPI_INT, &datasize);
+			
+			//si no recibo datos, y el tag es 1 significa que el maestro ha acabado de repartir sus datos
+			if(datasize == 0 && stStave.MPI_TAG == 1){
+				printf("================================= PROCESO %d TERMINA ====================== \n", rank);
+				seguir = 0;
+				break;
+			}
+			
+			//Recibimos los datos enviados por el maestro
+			MPI_Irecv(vector, VEC_SLAVE_SIZE, MPI_INT, ID_MAESTRO, stStave.MPI_TAG, MPI_COMM_WORLD, &req_1);
+			MPI_Wait(&req_1, &st_1);
+			printf("El proceso %d ha recibido datos.\n", rank);	
+					
 			//calcula el maximo del vector recibido
-			max = getMax(vectorEsclavo, VECTOR_SLAVE_SIZE);
-
-			//Envia el maximo calculado hacia el maestro.
-			isend_status = MPI_Isend(max, 1, MPI_INT, ID_MAESTRO, MESSAGE_TAG_0, MPI_COMM_WORLD, &request_2);
-			MPI_Wait(&request_2, &info_2);
-			//En este punto, este proceso ha terminado por lo que lo introducimos en la cola
-			colaProcesos[(indiceCola - 1) % size] = rank;
-
-			printf("El proceso %d ha terminado.\n", rank);
+			max = getMax(vector, VEC_SLAVE_SIZE);
+			printf("El proceso %d calcula el maximo %d.\n", rank, max);
+			
+			MPI_Isend(&max, 1, MPI_INT, ID_MAESTRO, 0, MPI_COMM_WORLD, &req_2);
+			MPI_Wait(&req_2, &st_2);
+			printf("El proceso %d envia el maximo al master.\n", rank);
+			
+			//El tag 1 indica que no hay mas datos por mandar
+			if(stStave.MPI_TAG == 1){
+				printf("================================= PROCESO %d TERMINA ====================== \n", rank);
+				seguir = 0;
+				break;
+			}
 		}
+	}	
+	
+		
 
-		if(rank == ID_MAESTRO){
-			//TODO hacer algo para que reciba los maximos
-			int irec_status = 0;
-			irec_status = MPI_Irecv(vectorEsclavo, VECTOR_SLAVE_SIZE, MPI_INT, colaProcesos[(indiceCola - 1) % size], MESSAGE_TAG_0, MPI_COMM_WORLD, &request_1);
+	if(rank == ID_MAESTRO){
+		while(restElems > 0){
+			//Comprobamos si hay algún mensaje pendiente
+			MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+			
+			//Obtenemos el proceso fuente del mensaje
+			source = status.MPI_SOURCE;
+			
+			MPI_Irecv(&max, 1, MPI_INT, source, 0, MPI_COMM_WORLD, &req_3);
+			MPI_Wait(&req_3, &st_3);
+			
+			printf("El maestro recibe del proceso %d el maximo %d. \n", source, max);
+			maximos[maxIndex] = max;
+			maxIndex++;
+			//Si solo queda un bloque de elementos por mandar, envio el tag 1
+			if(restElems - VEC_SLAVE_SIZE <= 0){
+				//Envia 1000 elementos de su vector
+				MPI_Isend(&vector[VEC_MASTER_SIZE - restElems], restElems, MPI_INT, source, 1, MPI_COMM_WORLD, &req_0);
+				MPI_Wait(&req_0, &st_0);
+				
+				printf("El maestro envia LOS ULTIMOS datos al proceso%d. \n", source);
+				max = getMax(&vector[VEC_MASTER_SIZE - restElems], VEC_SLAVE_SIZE);
+				printf("El master ha calculado el maximo del vector enviado al proceso %d: %d. \n", i, max);
+				//El maestro envia un mensaje vacio y con tag 1 al resto de procesos para que sepan que no quedan datos por mandar
+				for(i = 1; i < size; i++){
+					if(i != source){
+						MPI_Isend(0, 0, MPI_INT, i, 1, MPI_COMM_WORLD, &req_0);
+						MPI_Wait(&req_0, &st_0);
+					}	
+				}
+				
+			}else{
+				//Envia 1000 elementos de su vector
+				MPI_Isend(&vector[VEC_MASTER_SIZE - restElems], VEC_SLAVE_SIZE, MPI_INT, source, 0, MPI_COMM_WORLD, &req_0);
+				MPI_Wait(&req_0, &st_0);
+				printf("El maestro envia nuevos datos al proceso%d. \n", source);
+			}
+			
+			restElems = restElems - VEC_SLAVE_SIZE;
 		}
+		printf("========================= El maestro acaba de mandar sus  datos =========================\n");
+		
 	}
 	
+	MPI_Barrier(MPI_COMM_WORLD);
+	
+	if(rank == ID_MAESTRO){
+		globMax = getMax(maximos, VEC_MASTER_SIZE/size);
+		printf("========================= El maximo global es %d =========================\n", globMax);
+	}
 	
 	MPI_Finalize();
 	return 0;
@@ -131,4 +198,5 @@ int getMax(int * vector, int size){
 			max = vector[i];
 		}
 	}
+	return max;
 }
--- bloqueantes.c
+++ bloqueantes.c
@@ -4,16 +4,18 @@
 #include <time.h>
 
 	/*======================================================================================*
-	*		                    VARIABLES GLOBALES		                        *
+	*		                    		VARIABLES GLOBALES		                            *
 	*=======================================================================================*/
-
+//Numero que representa las filas y las columnas de la matriz del maestro
 #define MATRIX_SIZE 10000
+//ID del proceso maestro
 #define MASTER_ID 0
+//Maximo valor que pueden tomar los elementos de la matriz
 #define MAX_VALUE 100
 
 
 	/*======================================================================================*
-	*		                DECLARACION DE FUNCIONES		                *
+	*		               			 DECLARACION DE FUNCIONES				                *
 	*=======================================================================================*/
 
 void normalizeVetor(float * V, int length, int maxValue);
@@ -21,12 +23,12 @@ int findMax(float * vec, int size);
 
 int main(int argc, char *argv[])
 {
-	printf("Iniciando programa...\n");
+	//printf("Iniciando programa...\n");
 	MPI_Status info;
 	int rank, size, nRowsPerProcess, sendCount, i, j;
 	float ** matrix; 
-	float * matrix2;
-	float init_time, end_time, result, globalMax;
+	double init_time, end_time;
+	float result, globalMax;
 	time_t t;
 	
 	result = 0.0;
@@ -34,17 +36,14 @@ int main(int argc, char *argv[])
 
 	srand((unsigned) time(&t));
 
-	/*======================================================================================*
-	*        Cada proceso va a recivir (MATRIX_SIZE/Nº of process) columnas.	        *
-	*=======================================================================================*/
-
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD,&size);
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-
+	
+	//       Cada proceso va a recibir (MATRIX_SIZE/Nº of process) columnas.
+	
 	//Columnas que le tocan a cada proceso
 	nRowsPerProcess = MATRIX_SIZE/size;
-
 	//Numero de elementos que recibira cada proceso
 	sendCount = nRowsPerProcess * MATRIX_SIZE;
 
@@ -61,37 +60,23 @@ int main(int argc, char *argv[])
 				matrix[i][j] = rand()%(MAX_VALUE + 1);
 			}
 		}
-/*
-		printf("La matriz a repartir es: \n");
-		for(i = 0; i < MATRIX_SIZE; i++){
-			for(j = 0; j < MATRIX_SIZE; j++){
-				printf(" %f ", matrix[i][j]);
-			}
-			printf("\n");
-		}
-*/
+
 	}else{
-		printf("Hilo %d creando matriz...\n", rank);	
+		printf("Procesador %d creando matriz...\n", rank);	
 		//Reserva de filas
 		matrix = (float **)malloc(MATRIX_SIZE*sizeof(float*));
 			
-
 		//Si el proceso no es el master inicializa su matriz con ceros	
 		
 		for (i = 0; i < MATRIX_SIZE; i++){
 			matrix[i] = (float *) malloc (nRowsPerProcess*sizeof(float));
-			for (j = 0; j < nRowsPerProcess; j++){
-				matrix[i][j] = 0;
-			}
 		}
 		
 	}
 
-	
-
 	//Espero a todos los procesos
 	MPI_Barrier(MPI_COMM_WORLD); 
-
+	
 	init_time = MPI_Wtime();
 	
 	//Reparto la matriz del maestro entre todos los procesos 				
@@ -99,8 +84,11 @@ int main(int argc, char *argv[])
 	
 	//busco el maximo en el trozo que he recibido datos
 	result = findMax(&matrix[0][0], sendCount);
-	printf("El proceso %d mola, y ha calculado el maximo: %f ", rank, result);
+	printf("El proceso %d  ha calculado el maximo: %f ", rank, result);
 
+	//Espero a todos los procesos
+	MPI_Barrier(MPI_COMM_WORLD); 
+	
 	//El maximo global
 	MPI_Allreduce(&result, &globalMax, 1 , MPI_FLOAT, MPI_MAX, MPI_COMM_WORLD);
 	printf("El maximo global es: %f \n", globalMax);
@@ -108,28 +96,20 @@ int main(int argc, char *argv[])
 	normalizeVetor(&matrix[0][0], sendCount, globalMax);
 	
 	//Vuelo a juntar todos los datos en la matriz del maestro
+			//Matriz del maestro					//Matriz de los esclavos
 	MPI_Gather(&matrix[0][0], sendCount, MPI_FLOAT, &matrix[0][0], sendCount, MPI_FLOAT, MASTER_ID, MPI_COMM_WORLD);
 	
-
+	//Espero a todos los procesos
+	MPI_Barrier(MPI_COMM_WORLD); 
 	end_time = MPI_Wtime();	
 	
-
 	if(rank == MASTER_ID){
-	/*	printf("La matriz final es: \n");
-		for(i = 0; i < MATRIX_SIZE; i++){
-			for(j = 0; j < MATRIX_SIZE; j++){
-				printf(" %f ", matrix[i][j]);
-			}
-			printf("\n");
-		}*/
 		printf("Numero de procesos: [%d]. Tiempo empleado: [%f].\n", size, (end_time-init_time));
 	}
 	//free(matrix);
 	MPI_Finalize();
 	return 0;
 }
-
-
 
 
 void normalizeVetor(float * V, int length, int maxValue){
